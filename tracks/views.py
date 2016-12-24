3389caa7e3631fe5ad35209bec0f7a6b2633d44d
Luis Nieves <yosoy280@gmail.com>
2016-12-24 05:34:58 -0600

    Creacion de peticion JSON, solo para ejemplificar como se debe de hacer, se recomienda apoyarse en la documentacion sobre Serializadores

--- tracks/views.py
+++ tracks/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse, Http404
@@ -11,4 +12,25 @@ def track_view(request, title):
 	"""
 	track = get_object_or_404(Track, title=title)
 
-	return render(request, 'track.html', {'track': track})
+	#Respuesta JSON
+	data = {
+		'title': track.title,
+		'order': track.order,
+		'album': track.album,
+		'artist': {
+			'name': track.artist.first_name,
+			'biography': track.artist.biography,
+		}
+	}
+
+	# De JSON a cadena
+	json_data = json.dumps(data)
+
+	# De cadena a JSON
+	# json.loads(string_json)
+
+	# Hay que indicar al response que es un JSON, indicar tipo de peticion
+	# application/json para saber que envian o reciben un JSON
+	return HttpResponse(json_data, content_type='application/json')
+
+	# return render(request, 'track.html', {'track': track})
